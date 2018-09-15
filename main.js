42f039dd588c60135ea782f787cbd334ec2d0c26
Seshpenguin <seshan10@me.com>
2018-09-15 08:43:56 -0400

    Changes

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 
+// IPD Globals
+global.ipdUser = {username: null};
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
--- renderer.js
+++ renderer.js
@@ -1,3 +1,42 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var remote = require('electron').remote;
+remote.getGlobal('ipdUser').username = "sesh";
+
+var socket = io('http://localhost');
+socket.on('connect', function () {
+
+});
+socket.on('event', function (data) {
+
+});
+socket.on('disconnect', function () {
+
+});
+
+function logon() {
+    console.log(remote.getGlobal('ipdUser').username);
+}
+
+/* QEMU VM */
+
+const { spawn } = require('child_process');
+var vm;
+
+function startVM(image) { //sudo qemu-system-x86_64 -enable-kvm -m 4G -vga qxl -hda ubuntu.qcow2 -smp 4 -cpu host
+    vm = spawn('qemu-system-x86_64', ['-enable-kvm', '-m', '4G', '-vga', 'qxl', '-hda', '../ubuntu.qcow2', '-smp', '4', '-cpu', 'host']); // Start
+
+    vm.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    vm.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+    });
+
+    vm.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+
+}
