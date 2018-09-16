d947533f06eabe019f8dc608dd369db3a37dc24f
EspiDev <espidev@gmail.com>
2018-09-16 01:52:22 -0400

    fix

--- main.js
+++ main.js
@@ -134,15 +134,17 @@ expor.createVM = function (baseImage, createdname, image) {
 expor.loadVM = function (baseImageLocation, hash) {
     ipfsnode.start();
     let cid = new CID(hash);
-    ipfsnode.files.cat(cid, {}, function (err, file) { // TODO files.get instead?
+    ipfsnode.files.get(cid, function (err, files) { // TODO files.get instead?
         if (err) {
             throw err;
         }
-        fs.writeFile("../data.qcow2", file, (err) => {
-            if (err) return console.log(err);
-            require('child_process').exec("qemu-system-x86_64 -enable-kvm -m 4G -vga qxl -hda ../data.qcow2 -smp 4 -cpu host -spice port=5930,disable-ticketing -device virtio-serial-pci -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 -chardev spicevmc,id=spicechannel0,name=vdagent");
-            require('child_process').execSync("remote-viewer spice://127.0.0.1:5930");
-            ipfsnode.stop();
+        files.forEach((file) => {
+            fs.writeFile("../data.qcow2", file, (err) => {
+                if (err) return console.log(err);
+                require('child_process').exec("qemu-system-x86_64 -enable-kvm -m 4G -vga qxl -hda ../data.qcow2 -smp 4 -cpu host -spice port=5930,disable-ticketing -device virtio-serial-pci -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 -chardev spicevmc,id=spicechannel0,name=vdagent");
+                require('child_process').execSync("remote-viewer spice://127.0.0.1:5930");
+                ipfsnode.stop();
+            });
         });
     });
 };
